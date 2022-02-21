c3e3b9903057f30e34d8ffd9c6a6295829d57190
mdeboute <martin.deboute@gmail.com>
2022-02-21 12:18:07 +0100

    update the script

--- extensions.sh
+++ extensions.sh
@@ -2,31 +2,40 @@
 
 # C/C++
 code --install-extension ms-vscode.cpptools # provided by microsoft
-# code --install-extension jbenden.c-cpp-flylint # linting based on clang & cppcheck
+code --install-extension jbenden.c-cpp-flylint # linting based on clang & cppcheck
+code --install-extension jeff-hykin.better-cpp-syntax
+code --install-extension ms-vscode.cpptools-extension-pack
+code --install-extension ms-vscode.cpptools-themes
+code --install-extension austin.code-gnu-global
 
 # Python (provided by Microsoft) + Pylance + Jupyter Notebook
 code --install-extension ms-python.python
 code --install-extension ms-python.vscode-pylance
 code --install-extension ms-toolsai.jupyter
+code --install-extension ms-toolsai.jupyter-keymap
+code --install-extension ms-toolsai.jupyter-renderers
 
 # Java + Maven (pack)
 code --install-extension vscjava.vscode-java-pack
 code --install-extension vscjava.vscode-java-dependency
 code --install-extension vscjava.vscode-maven
 code --install-extension vscjava.vscode-java-test
-code --insert-extension vscjava.vscode-java-debug
+code --install-extension vscjava.vscode-java-debug
 code --install-extension redhat.java
 
 # External libraries as symlinks
 code --install-extension dannielarriola.extlibraries
 
-# code runner
+# Code runner
 code --install-extension formulahendry.code-runner
 
-# Git + Github
+# Git + Github + Gitlab
 code --install-extension eamodio.gitlens
 code --install-extension donjayamanne.githistory
+code --install-extension felipecaputo.git-project-manager
 code --install-extension github.vscode-pull-request-github
+code --install-extension GitLab.gitlab-workflow
+code --install-extension mhutchie.git-graph
 
 # Markdown support
 code --install-extension yzhang.markdown-all-in-one
@@ -36,15 +45,16 @@ code --install-extension evilz.vscode-reveal # show markdown as revealJs present
 # LaTeX
 code --install-extension James-Yu.latex-workshop
 
-# Doxygen Documentation Generator
+# Documentation Generator
 code --install-extension cschlosser.doxdocgen
 code --install-extension bbenoist.Doxygen
+code --install-extension njpwerner.autodocstring
 
 # CMake & CMake Tools
 code --install-extension twxs.cmake
 code --install-extension ms-vscode.cmake-tools
 code --install-extension cheshirekow.cmake-format
-# code --install-extension maddouri.cmake-tools-helper # useful or not?
+code --install-extension maddouri.cmake-tools-helper # useful or not?
 
 # Google Tests
 code --install-extension DavidSchuldenfrei.gtest-adapter
@@ -56,15 +66,14 @@ code --install-extension abhinavk99.y86-vscode # formatting
 code --install-extension HookyQR.beautify
 
 # Node.js (Javascript, NPM, ...)
-# code --install-extension dbaeumer.vscode-eslint
-# code --install-extension eg2.vscode-npm-script
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension eg2.vscode-npm-script
 
 # CSS
-# code --install-extension aeschli.vscode-css-formatter
-# code --install-extension Zignd.html-css-class-completion
+code --install-extension aeschli.vscode-css-formatter
+code --install-extension Zignd.html-css-class-completion
 
 # Spell Right (buggy?)
-
 # english / french language support (spell right)
 # code --install-extension ban.spellright
 # sudo apt-get install hunspell-fr hunspell-en-us       # Linux
@@ -83,12 +92,15 @@ code --install-extension christian-kohler.path-intellisense # path auto-completi
 # Wakatime
 code --install-extension wakatime.vscode-wakatime
 
-# AI intellicode
+# AI/intellicode
 code --install-extension visualstudioexptteam.vscodeintellicode
+code --install-extension GitHub.copilot
+code --install-extension VisualStudioExptTeam.intellicode-api-usage-examples
 
 # Remote ssh vscode
 code --install-extension ms-vscode-remote.remote-ssh-edit
 code --install-extension ms-vscode-remote.remote-ssh
+code --install-extension ms-vscode-remote.remote-containers
 
 # Themes
 code --install-extension dracula-theme.theme-dracula
@@ -100,3 +112,13 @@ code --install-extension thenikso.github-plus-theme
 
 # Sync your vscode settings
 code --install-extension Shan.code-settings-sync
+
+# DB management + Docker
+code --install-extension bajdzis.vscode-database
+code --install-extension ms-azuretools.vscode-docker
+
+# Excel viewer
+code --install-extension GrapeCity.gc-excelviewer
+
+# vscode French language pkg
+code --install-extension MS-CEINTL.vscode-language-pack-fr
